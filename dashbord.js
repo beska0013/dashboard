db3462583eccc777a42b1677bfa9a1f64f4f3d5e
Besiki Gugushvili <74554872+beska0013@users.noreply.github.com>
2020-12-07 05:03:31 +0400

    navbar in progres

---
+++ dashbord.js
@@ -0,0 +1,18 @@
+document.getElementById("cusBtn").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("cusDropdown").classList.toggle("appear");
+  }
+
+
+document.getElementById("cusBtn2").onclick = function() {myFunction2()};
+
+function myFunction2() {
+    document.getElementById("cusDropdown2").classList.toggle("appear2");
+  }
+
+document.getElementById("cusBtn3").onclick = function() {myFunction3()};
+
+function myFunction3() {
+    document.getElementById("cusDropdown3").classList.toggle("appear2");
+  }
