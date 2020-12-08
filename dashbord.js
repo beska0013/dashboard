9e54989218f5514cb99a14d52d52f2bcbaa64439
Besiki Gugushvili <74554872+beska0013@users.noreply.github.com>
2020-12-08 23:44:48 +0400

    08.12.20 in progres

--- dashbord.js
+++ dashbord.js
@@ -1,18 +1,62 @@
-document.getElementById("cusBtn").onclick = function() {myFunction()};
+// mega menu dropdown//
 
-function myFunction() {
-    document.getElementById("cusDropdown").classList.toggle("appear");
-  }
+ const megaBtn = document.getElementById('megaBtn')
+ const megaMenu = document.getElementById('megaMenu')
+ 
+ 
+ function closeMegaMenu(e) {
+   if(e.target == megaBtn) {
+     megaMenu.classList.toggle('appear')
+   }
+   else {
+     megaMenu.classList.remove('appear')
+   }
+ }
+
+ document.addEventListener('click', closeMegaMenu)
+
+ //mega menu dropdown end//
+
+ //right navbar menu dropdown//
+
+ const modalBtn = document.getElementById('modalBtn')
+ const modalDropdown = document.getElementById('modal')
+ 
+ 
+ function closeModalMenu(e) {
+   if(e.target == modalBtn) {
+     modalDropdown.classList.toggle('appear2')
+   }
+   else {
+     modalDropdown.classList.remove('appear2')
+   }
+ }
+
+ document.addEventListener('click', closeModalMenu)
+
+ const modalBtn2 = document.getElementById('modalBtn2')
+ const modalDropdown2 = document.getElementById('modal2')
+ 
+ 
+ function closeModalMenu2(e) {
+   if(e.target == modalBtn2) {
+     modalDropdown2.classList.toggle('appear2')
+   }
+   else {
+     modalDropdown2.classList.remove('appear2')
+   }
+ }
+
+ document.addEventListener('click', closeModalMenu2)
 
 
-document.getElementById("cusBtn2").onclick = function() {myFunction2()};
 
-function myFunction2() {
-    document.getElementById("cusDropdown2").classList.toggle("appear2");
-  }
 
-document.getElementById("cusBtn3").onclick = function() {myFunction3()};
 
-function myFunction3() {
-    document.getElementById("cusDropdown3").classList.toggle("appear2");
-  }
+
+
+// document.getElementById("cusBtn3").onclick = function () { myFunction3() };
+
+// function myFunction3() {
+//   document.getElementById("cusDropdown3").classList.toggle("appear2");
+// }
